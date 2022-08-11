fac23b7d6688176adb865ee4e1080313ef52016b
Bassman59 <111052029+Bassman59@users.noreply.github.com>
2022-08-11 13:21:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title ('My new healthy Diner')
 
 streamlit.header ('Lunchtime Digger Bro')
 
-streamlit.text ('Pizza SalaLmi.... but Frappe before')
+streamlit.text ('Pizza Salami.... but Frappe before')
 
 
