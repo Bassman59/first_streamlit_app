4f1b92d0972505c20fa62cc642fa07002bbb6d97
Bassman59 <111052029+Bassman59@users.noreply.github.com>
2022-08-11 13:29:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.header('🥣Breakfast Menu')
 streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
-streamlit.text(🐔'Hard-Boiled Free-Range Egg')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
 
 
